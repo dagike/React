fd56d74f19b448bc0d3dce99ae60ec79f1f0387c
DAGIKE <isaac20053@hotmail.com>
2019-03-23 18:56:49 -0400

    finish blog section 15

---
+++ 07_blog/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import postsReducers from "./postsReducer";
+import usersReducers from "./usersReducer";
+
+export default combineReducers({
+  posts: postsReducers,
+  users: usersReducers
+});
