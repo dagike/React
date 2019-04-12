4eb042c558341c6af174a7188c255f7bddf046a8
DAGIKE <isaac20053@hotmail.com>
2019-04-12 19:59:01 -0400

    add programmatic to StreamCreate action

--- 08_streams/client/src/actions/index.js
+++ 08_streams/client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
   EDIT_STREAM,
   DELETE_STREAM
 } from "./types";
+import history from "../history";
 
 export const signIn = userId => {
   return {
@@ -30,6 +31,8 @@ export const createStream = formValues => async (dispatch, getState) => {
     type: CREATE_STREAM,
     payload: response.data
   });
+
+  history.push("/");
 };
 
 export const getStreams = () => async dispatch => {
--- 08_streams/client/src/components/App.js
+++ 08_streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
@@ -7,12 +7,13 @@ import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
+import history from "../history";
 
 class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <BrowserRouter>
+        <Router history={history}>
           <div>
             <Header />
             <Route path="/" exact component={StreamList} />
@@ -21,7 +22,7 @@ class App extends Component {
             <Route path="/streams/delete" exact component={StreamDelete} />
             <Route path="/streams/show" exact component={StreamShow} />
           </div>
-        </BrowserRouter>
+        </Router>
       </div>
     );
   }
