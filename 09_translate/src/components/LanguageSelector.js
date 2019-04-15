878901a13944ddd2b98c7a7db40d9eadbe3ee2dd
DAGIKE <isaac20053@hotmail.com>
2019-04-14 22:27:07 -0400

    refactor to use LanguageStore

--- 09_translate/src/components/LanguageSelector.js
+++ 09_translate/src/components/LanguageSelector.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class LanguageSelector extends Component {
+  static contextType = LanguageContext;
+
   render() {
     return (
       <div>
         Select a language:
         <i
-          onClick={() => this.props.onLanguageChange("english")}
+          onClick={() => this.context.onLanguageChange("english")}
           className="flag us"
         />
         <i
-          onClick={() => this.props.onLanguageChange("dutch")}
+          onClick={() => this.context.onLanguageChange("dutch")}
           className="flag nl"
         />
       </div>
