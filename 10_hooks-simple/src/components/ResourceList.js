c7b622eb0e92b586851635cdab36e36723927759
DAGIKE <isaac20053@hotmail.com>
2019-04-15 12:50:21 -0400

    reallocate Hooks to a new component to reuse

--- 10_hooks-simple/src/components/ResourceList.js
+++ 10_hooks-simple/src/components/ResourceList.js
@@ -1,18 +1,5 @@
-import React, { useState, useEffect } from "react";
-import jsonPlaceholder from "../api/jsonPlaceholder";
-
-const useResources = resource => {
-  const [resources, setResources] = useState([]);
-
-  useEffect(() => {
-    (async resource => {
-      const res = await jsonPlaceholder.get(`/${resource}`);
-      setResources(res.data);
-    })(resource);
-  }, [resource]);
-
-  return resources;
-};
+import React from "react";
+import useResources from "./useResources";
 
 export default ({ resource }) => {
   const resources = useResources(resource);
